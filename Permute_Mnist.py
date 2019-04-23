83270c113ad17fec55b7e62cb2894a7f76821a0f
“Rahaf <rahaf.aljundi@gmail.com>
2019-04-23 18:33:46 +0200

    req

--- Permute_Mnist.py
+++ Permute_Mnist.py
@@ -1,5 +1,8 @@
 from Permuted_MNIST import *
+import os
 def create_datasets(path='./Datasets'):
+    if not os.path.isdir(path):
+        os.mkdir(path)
     num_task=5
     all_dsets=get_permute_mnist(num_task=num_task)
     for t in range(0,num_task):
